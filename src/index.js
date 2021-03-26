43c426978891e3a151ec54d83fa184e9622d660d
Igor <62031924+SpaceHolderdJs@users.noreply.github.com>
2021-03-26 16:11:40 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "../js/main.js";
+import "../node_modules/jquery/dist/jquery.js";
+import "../styles/main.css";
+import "../node_modules/materialize-css/dist/js/materialize.min.js";
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
