c6bf1a66bc08b568441dad3462b92a3820465adc
Igor <62031924+SpaceHolderdJs@users.noreply.github.com>
2021-03-26 16:10:09 +0200

    Delete .gitignore directory

--- .gitignore/HomeTask39/src/index.js
+++
@@ -1,5 +0,0 @@
-import "../js/main.js";
-import "../node_modules/jquery/dist/jquery.js";
-import "../styles/main.css";
-import "../node_modules/materialize-css/dist/js/materialize.min.js";
-import "../node_modules/materialize-css/dist/css/materialize.min.css";
